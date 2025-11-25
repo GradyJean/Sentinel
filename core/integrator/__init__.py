200f680b28bbac8c747067f2c7c130cf8b3c3594
GradyJean <grady.jean.king@gmail.com>
2025-11-25 14:16:37 +0800

    修改配置文件

--- core/integrator/__init__.py
+++ core/integrator/__init__.py
@@ -1,4 +1,4 @@
 from core.integrator.nginx import Nginx
 import config
 
-nginx = Nginx(config.settings.nginx_base_path, config.CORE_OS)
+nginx = Nginx(config.settings.nginx.base_path, config.CORE_OS)
--- models/config.py
+++ models/config.py
@@ -9,25 +9,27 @@ class ServerConfig(BaseModel):
     """
     服务配置
     """
-    host: str
-    port: int
-    base_path: str
-    static_path: str
+    host: str = "0.0.0.0"
+    port: int = 8080
+    base_path: str = ""
+    static_path: str = ""
 
 
 class NginxConfig(BaseModel):
     """
     Nginx 配置
     """
-    base_path: str
-    log_path: str
-    conf_path: str
+    base_path: str = ""
+    log_path: str = ""
+    conf_path: str = ""
+    black_list_file: str = ""
+    rate_limit_file: str = ""
 
 
 class ElasticsearchConfig(BaseModel):
     """
     Elasticsearch 配置
     """
-    url: str
-    username: str
-    password: str
+    url: str = ""
+    username: str = ""
+    password: str = ""
